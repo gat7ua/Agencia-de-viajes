f0842d004c92b2a84c06acfd31e7bf6c64900c84
gat7 <gat7@alu.ua.es>
2021-06-24 21:06:04 +0200

    mas cosas

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-var http = require('http');
+'use strict'
+
 var puerto = 8080
 
-http.createServer( (request, response) => {
+const express = require('express');
+const app = express();
+
+app.get('/hola', (request, response) => {
+    response.send('Tu puta madre\n');
+})
+
+app.listen(puerto, () => {
+    console.log('API REST en http://localhost:' + puerto + '/hola');
+});
+
+/*
+var http = require('http');
+var server = http.createServer();
+
+function HTTP_Response(request, response) {
     response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.end('PUTA\n');
-}).listen(puerto);
+    response.write('TODAS PUTAS, TODOS PUTOS')
+    response.end();
+}
+
+server.on('request', HTTP_Response);
+server.listen(puerto);
 
 console.log('Servidor ejecutandose en ' + puerto + '...');
+*/
