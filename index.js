76f523a31a52ce701c25fd5e12e6642bbfbd4af7
gat7 <gat7@alu.ua.es>
2021-06-24 22:12:51 +0200

    Mas cosilla

--- index.js
+++ index.js
@@ -1,30 +1,194 @@
 'use strict'
 
-var puerto = 8080
+//Puerto
+const port = process.env.PORT || 3000;
+
+const https = require('https');
+const fs = require('fs');
+
+const OPTIONS_HTTPS = {
+    key: fs.readFileSync('./cert/key.pem'),
+    cert: fs.readFileSync('./cert/cert.pem')
+};/**/
 
 const express = require('express');
+const logger = require('morgan');
+const mongojs = require('mongojs');
+
+
 const app = express();
 
-app.get('/hola', (request, response) => {
-    response.send('Tu puta madre\n');
-})
+//var db = mongojs('localhost:27017/SD'); //Conectamos con la DB
+var db = mongojs('mongodb+srv://est20:1234@cluster0.dm1rq.mongodb.net/vuelo?retryWrites=true&w=majority');
+var id = mongojs.ObjectID;
 
-app.listen(puerto, () => {
-    console.log('API REST en http://localhost:' + puerto + '/hola');
+//Middleware
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.param("colecciones", (request, response, next, colecciones) => {
+    console.log('middleware param /api/:colecciones');
+    request.collection = db.collection(colecciones);
+    return next();
 });
 
-/*
-var http = require('http');
-var server = http.createServer();
+function auth(request, response, next) {
+    if (!request.headers.authorization) {
+        response.status(401).json({
+            result: 'KO',
+            mensaje: "No se ha enviado el token tipo Bearer en la cabecera Authorization"
+        })
+        return next(new Error("Falta token de autorizacion"));
+    }
 
-function HTTP_Response(request, response) {
-    response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.write('TODAS PUTAS, TODOS PUTOS')
-    response.end();
+    console.log(request.headers.authorization);
+    if (request.headers.authorization.split(" ")[1] === "MITOKEN123456789") {
+        return next();
+    }
+
+    response.status(401).json({
+        result: 'KO',
+        mensaje: "Acceso no autorizado a este servicio"
+    });
+
+    return next(new Error("Acceso no autorizado"));
 }
 
-server.on('request', HTTP_Response);
-server.listen(puerto);
+//RESERVAS
+app.post('/api/reserva/:id', auth, (request, response, next) => {
+    const queID = request.params.id;
+    var collection = db.collection("reserva");
 
-console.log('Servidor ejecutandose en ' + puerto + '...');
-*/
+    collection.save({ '_id': queID }, (err, elementoGuardado) => {
+        if (err) return next(err);
+        console.log(elementoGuardado);
+        response.status(201).json({
+            result: 'OK',
+            elemento: elementoGuardado
+        });
+    });
+});
+
+//Declaramos nuestras rutas y nuestros controladores
+app.get('/api', (request, response, next) => {
+    db.getCollectionNames((err, colecciones) => {
+        if (err) return next(err); //Propagamos el error
+        console.log(colecciones);
+        response.json({
+            result: 'OK',
+            colecciones: colecciones
+        });
+    });
+});
+
+//GET coleccion
+app.get('/api/:colecciones', (request, response, next) => {
+    const queColeccion = request.params.colecciones;
+
+    request.collection.find((err, elementos) => {
+        if (err) return next(err);
+        console.log(elementos);
+        response.json({
+            result: 'OK',
+            colecciones: queColeccion,
+            elementos: elementos
+        });
+    });
+});
+
+//GET item por id
+app.get('/api/:colecciones/:id', (request, response, next) => {
+    const queColeccion = request.params.colecciones;
+    const queId = request.params.id;
+    request.collection.findOne({ _id: id(queId) }, (err, elemento) => {
+        if (err) return next(err);
+        console.log(elemento);
+        response.json({
+            result: 'OK',
+            colecciones: queColeccion,
+            elementos: elemento
+        });
+    });
+});
+
+//POST elemento
+app.post('/api/:colecciones', auth, (request, response, next) => {
+    const nuevoElemento = request.body;
+    const queColeccion = request.params.colecciones;
+
+    if (queColeccion == "reserva") {
+        console.log(nuevoElemento.idProveedor);
+        const queID = JSON.stringify(nuevoElemento.idProveedor);
+        console.log(queID);
+        request.collection.findOne({ "idProveedor": nuevoElemento.idProveedor }, (err, elemento) => {
+
+            if (elemento != null && elemento.idProveedor == nuevoElemento.idProveedor) {
+                response.json(`Error: reserva ya realizada`);
+            } else {
+                request.collection.save(nuevoElemento, (err, elementoGuardado) => {
+                    if (err) return next(err);
+
+                    console.log(elementoGuardado);
+                    response.status(201).json({
+                        result: 'OK',
+                        coleccion: queColeccion,
+                        elemento: elementoGuardado
+                    });
+                });
+            }
+        });
+    }
+    else {
+        request.collection.save(nuevoElemento, (err, elementoGuardado) => {
+            if (err) return next(err);
+
+            console.log(elementoGuardado);
+            response.status(201).json({
+                result: 'OK',
+                coleccion: queColeccion,
+                elemento: elementoGuardado
+            });
+        });
+    }
+});
+
+//PUT elemento por su id
+app.put('/api/:colecciones/:id', auth, (request, response, next) => {
+    const queColeccion = request.params.colecciones;
+    const nuevosDatos = request.body;
+    const queId = request.params.id;
+    request.collection.update(
+        { _id: id(queId) },
+        { $set: nuevosDatos },
+        { safe: true, multi: false },
+        (err, resultado) => {
+            if (err) return next(err);
+            console.log(resultado);
+            response.json({
+                result: 'OK',
+                coleccion: queColeccion,
+                resultado: resultado
+
+            });
+        }
+    );
+});
+
+//DELETE elemento pos su id
+app.delete('/api/:colecciones/:id', auth, (request, response, next) => {
+    const queColeccion = request.params.colecciones;
+    const queId = request.params.id;
+    request.collection.remove(
+        { _id: id(queId) },
+        (err, resultado) => {
+            if (err) return next(err);
+            response.json(resultado);
+        }
+    );
+});
+
+//Inicialización del servidor
+https.createServer(OPTIONS_HTTPS, app).listen(port, () => {
+    console.log('SEC WS API REST CRUD con DB ejecutandose en https://localhost:' + port + '/:colecciones/:id')
+});
